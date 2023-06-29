9ced6f17dffd9bff26bba24e030a9fd35f15bd23
David Derrick Anyuru <davidderrickanyuru@gmail.com>
2023-06-29 11:42:55 +0300

    Directory structure and initial plugin setup

---
+++ wp-content/plugins/wp-admin-todo-plugin/wp_admin_todo.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+* Admin todo plugin
+*
+* @package           Todo Plugin
+* @author            PHP Ninjas
+* @copyright         2019 Your Name or Company Name
+* @license           GPL-2.0-or-later
+*
+* @wordpress-plugin
+* Plugin Name:       Todo Plugin
+* Plugin URI:        https://github.com/techlogug/To-do-WordPress-plugin
+* Description:       A plugin to manage admin todos
+* Version:           1.0.0
+* Requires at least: 5.2
+* Requires PHP:      7.2
+* Author:            PHP ninjas
+* Author URI:        https://github.com/techlogug/To-do-WordPress-plugin
+* License:           GPL v2 or later
+* License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+* Update URI:        https://github.com/techlogug/To-do-WordPress-plugin
+*/
+
+?>
